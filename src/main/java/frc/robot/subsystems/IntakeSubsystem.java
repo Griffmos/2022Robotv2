4179b58a5a00fd6462493e61ac7233d778f4b554
Adikesh Nathan <88110937+Turtlez200@users.noreply.github.com>
2022-09-30 18:23:28 -0400

    Finished IntakeSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,14 +4,53 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new Intake. */
-  public Intake() {}
+
+  private WPI_TalonFX intakeMotor = new WPI_TalonFX(11); // Located popping out in the front of the robot
+  private double currIntakePercOut = 0; // Used to store the current percent output of intake motor
+
+  private WPI_TalonFX gateMotor = new WPI_TalonFX(9); // Near the top of the robot
+  private double currGatePercOut = 0; // Used to store the current percent output of gate motor
+
+  public IntakeSubsystem() {
+    intakeMotor.configFactoryDefault();
+    gateMotor.configFactoryDefault();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    SmartDashboard.putNumber("INTAKE MOTOR PERCENT OUTPUT", currIntakePercOut);
+    SmartDashboard.putNumber("GATE MOTOR PERCENT OUTPUT", currGatePercOut);
+
   }
+
+  // Change Speed to the Intake and Gate Motors
+  public void set(double intakeSpeed, double gateSpeed) {
+
+    intakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
+    gateMotor.set(ControlMode.PercentOutput, gateSpeed);
+    currIntakePercOut = intakeSpeed;
+    currGatePercOut = gateSpeed;
+
+  }
+
+  // Stop all motors and reset variables
+  public void stop() {
+
+    intakeMotor.set(ControlMode.PercentOutput, 0);
+    gateMotor.set(ControlMode.PercentOutput, 0);
+    currIntakePercOut = 0;
+    currGatePercOut = 0;
+
+  }
+
 }
